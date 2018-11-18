62809b1f2da02720ed7c9a979460da03a3c43baa
caktopik <taufik.arief.widodo@gmail.com>
2018-11-18 17:58:06 +0700

    updated 18 nov 18 creating profile page

---
+++ application/modules/profile/controllers/Profile.php
@@ -0,0 +1,29 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class Profile_admin extends Admin_Controller 
+{
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->library('template');
+        $this->load->helper('adminlte_helper');
+
+        // load ion auth
+        $this->load->add_package_path(APPPATH.'third_party/ion_auth/');
+        $this->load->library('ion_auth');
+        if(!$this->ion_auth->logged_in())
+        {
+             redirect('auth', 'refresh');
+        }
+    }
+
+    public function index()
+	{
+        $data = array();
+        $data['page_title'] = 'User Profile';
+        $data['page_description'] = '';
+        
+        $this->template->_render_admin('profile_index', $data);   
+    }
+}
